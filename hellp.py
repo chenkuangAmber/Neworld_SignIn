873200e56ae0397ebe9e53b0ef07958e4803cdc1
DDie <31473119+chenkuangAmber@users.noreply.github.com>
2020-12-02 19:20:47 +0800

    Update hellp.py

--- hellp.py
+++ hellp.py
@@ -79,4 +79,4 @@ driver.refresh()#刷新页面
 # buttons = driver.find_element_by_xpath("//button[@id='checkin']")
 # print('buttons',buttons)
 
-# driver.find_element_by_id("checkin").click() # 点击元素
+driver.find_element_by_id("checkin").click() # 点击元素
