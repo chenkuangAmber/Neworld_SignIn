d62ec5aeadba958da7125a20566ca29802ed60e8
chenkuang <416811497@qq.com>
2020-12-02 09:33:18 +0800

    第一次提交     添加可登录并点击签到功能

---
+++ hellp.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+import time
+import json
+
+driver = webdriver.Chrome("D:\Downloads\chromedriver_win32\chromedriver.exe")    # Chrome浏览器
+
+# driver.deleted_all_cookies()
+
+# driver.get('http://www.baidu.com') 
+driver.get('https://neworld.date/user') 
+#  获取cookies 
+# time.sleep(10)
+# cookies = driver.get_cookies()
+# print (type(cookies))
+# # print ("".join(cookies))
+# f1 = open('cookie.txt', 'w')
+# f1.write(json.dumps(cookies))
+
+f1 = open('cookie.txt')
+cookie = f1.read()
+cookie =json.loads(cookie)
+for c in cookie:
+    driver.add_cookie(c)
+
+driver.refresh()#刷新页面 
+
+driver.find_element_by_id("checkin").click() # 点击元素
